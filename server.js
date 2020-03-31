46fa49ba6e39225e9696a1d0c6069fa15bed7188
novak254 <ndemokelvinonkundi@gmail.com>
2020-03-31 15:45:30 +0300

    update server.js with correct path names

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/dashboard-app'));
+app.use(express.static(__dirname + '/dist/DashboardApp'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/dashboard-app/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/DashboardApp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
